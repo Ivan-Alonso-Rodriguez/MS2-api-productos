2c38cfcdca2171c770784d82b80419ba32baee82
Ivan Rodriguez <ivan.rodriguez@utec.edu.pe>
2025-06-10 19:57:23 -0500

    Update crearProducto.js

--- lambdas/crearProducto.js
+++ lambdas/crearProducto.js
@@ -4,7 +4,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 const s3 = new AWS.S3();
 
 module.exports.crearProducto = async (event) => {
-  const validacion = validarToken(event.headers);
+  const validacion = await validarToken(event.headers);
   if (!validacion.ok) return validacion.respuesta;
 
   const { codigo, nombre, descripcion, precio, imagen_base64 } = JSON.parse(event.body);
@@ -12,7 +12,7 @@ module.exports.crearProducto = async (event) => {
   let imagen_url = null;
   if (imagen_base64) {
     const buffer = Buffer.from(imagen_base64, 'base64');
-    const params = {
+    const s3Params = {
       Bucket: 'ms2-productos-imgs',
       Key: `${codigo}.jpg`,
       Body: buffer,
@@ -20,26 +20,32 @@ module.exports.crearProducto = async (event) => {
       ContentType: 'image/jpeg',
       ACL: 'public-read'
     };
-    await s3.putObject(params).promise();
+    await s3.putObject(s3Params).promise();
     imagen_url = `https://ms2-productos-imgs.s3.amazonaws.com/${codigo}.jpg`;
   }
 
+  const producto = {
+    codigo,
+    tenant_id: validacion.datos.tenant_id,
+    user_id: validacion.datos.user_id,
+    nombre,
+    descripcion,
+    precio,
+    ...(imagen_url && { imagen_url })
+  };
+
   const paramsDynamo = {
     TableName: 't_MS2_productos',
-    Item: {
-      codigo,
-      tenant_id: validacion.datos.tenant_id,
-      nombre,
-      descripcion,
-      precio,
-      ...(imagen_url && { imagen_url })
-    }
+    Item: producto
   };
 
   await dynamodb.put(paramsDynamo).promise();
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ msg: 'Producto creado', imagen_url })
+    body: JSON.stringify({
+      mensaje: 'Producto creado correctamente',
+      producto
+    })
   };
 };
